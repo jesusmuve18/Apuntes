0c01e0de62d120b57ecfb88fa20c729734fb3f6f
jesusmuve18 <jesumuve19@gmail.com>
2024-10-09 03:05:29 +0200

    Actualizado 09.10.2024 03:05

---
+++ otros/script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    let bloques = document.querySelectorAll("li a");
+
+    for(let i=0; i<bloques.length; i++){
+
+        if(bloques[i].getAttribute("href")==""){
+            bloques[i].innerHTML = 
+            "<img class=\"icon-left\" src=\"icons/folder_icon.svg\" alt=\"Carpeta\">"
+            + bloques[i].innerHTML +
+            "<img class=\"icon-right\" src=\"icons/arrow_icon.svg\" alt=\"flecha\"></img>";
+        } else {
+            bloques[i].innerHTML = 
+            "<img class=\"icon-left\" src=\"icons/link_icon.svg\" alt=\"Enlace\">"
+            + bloques[i].innerHTML;
+        }
+    }
+};
+
