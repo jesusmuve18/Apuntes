20e175300437b6ba6cc54aafc25e8dd63e2eb8c0
jesusmuve18 <jesumuve19@gmail.com>
2024-11-19 18:00:26 +0100

    Actualizado 19.11.2024 18:00

--- code/SCD/Practicas/Practica_2/lec_escr_mu.cpp
+++ code/SCD/Practicas/Practica_2/lec_escr_mu.cpp
@@ -1,19 +1,3 @@
-// -----------------------------------------------------------------------------
-//
-// Sistemas concurrentes y Distribuidos.
-// Práctica 2. Introducción a los monitores en C++11.
-//
-// Archivo: prodcons1_su.cpp
-//
-// Ejemplo de un monitor en C++11 con semántica SU, para el problema
-// del productor/consumidor, con productor y consumidor únicos.
-// Opcion LIFO
-//
-// Historial:
-// Creado el 30 Sept de 2022. (adaptado de prodcons2_su.cpp)
-// 20 oct 22 --> paso este archivo de FIFO a LIFO, para que se corresponda con lo que dicen las transparencias
-// -----------------------------------------------------------------------------------
-
 #include <iostream>
 #include <iomanip>
 #include <cassert>
@@ -26,10 +10,10 @@ using namespace scd;
 
 constexpr int
     num_items = 5,      // número de items a del array
-    num_escritores = 5, // número de hebras consumidoras
-    num_lectores = 5;   // número de hebras productoras
+    num_escritores = 5, // número de escritores
+    num_lectores = 5;   // número de lectores
 
-int cuanto_leo = 5,       // cuantos lectores pueden entrar a leer desde que un escritor entra a la cola de escritura
+int tam_cola_lec = 5,     // número de lectores que pueden leer la entrada de un escritor a la cola de escritura
     pergamino[num_items]; // siguiente valor a devolver en 'producir_dato'
 
 constexpr int
@@ -38,28 +22,24 @@ constexpr int
 
 mutex
     mtx; // mutex de escritura en pantalla
-//**********************************************************************
-// funciones
-//----------------------------------------------------------------------
 
 // *****************************************************************************
-// clase para monitor buffer, version FIFO, semántica SC, multiples prod/cons
-
 class LecEscMU1 : public HoareMonitor
 {
 private:
-    static const int              // constantes ('static' ya que no dependen de la instancia)
-        num_celdas_total = 10;    //   núm. de entradas del buffer
-    int                           // variables permanentes
-        buffer[num_celdas_total], //   buffer de tamaño fijo, con los datos
-        n_lec;                    //   numero de lectores leyendo
-    bool escrib = false;          // si hay un escritor escribiendo
+    static const int             
+        num_celdas_total = 10;      // tamaño del buffer
+    int                           
+        buffer[num_celdas_total],   // buffer de datos
+        n_lec;                      // número de lectores leyendo
+    bool 
+        escrib = false;            // true si hay un escritor escribiendo
 
-    CondVar        // colas condicion:
-        lectura,   //  cola donde espera el consumidor (n>0)
-        escritura; //  cola donde espera el productor  (n<num_celdas_total)
+    CondVar       
+        lectura,                //  cola donde esperan los lectores (n>0)
+        escritura;              //  cola donde esperan los ecritores  (n<num_celdas_total)
 
-public:                   // constructor y métodos públicos
+public: 
     LecEscMU1();          // constructor
     void ini_lectura();   //  función llamada por el lector para leer
     void fin_lectura();   //  función llamada por el lector para finalizar lectura
@@ -67,79 +47,83 @@ public:                   // constructor y métodos públicos
     void fin_escritura(); //
 };
 // -----------------------------------------------------------------------------
+// Constructor de la clase LecEscMU1
 
 LecEscMU1::LecEscMU1()
 {
-    n_lec = 0;                // buffer vacío
-    escrib = false;           // buffer vacío
-    lectura = newCondVar();   // inicialmente no hay nada que leer
-    escritura = newCondVar(); // inicialmente no hay nada que escribir
+    n_lec = 0;
+    escrib = false;          
+    lectura = newCondVar(); 
+    escritura = newCondVar();
 }
 // -----------------------------------------------------------------------------
-// función llamada por el lector para leer
+// Función llamada por el lector para leer
 
 void LecEscMU1::ini_lectura()
 {
-    // esperar bloqueado hasta que 0 < n_lec
-    if (escrib || cuanto_leo == 0)
+    // esperar bloqueado hasta que 0 < n_lec o hasta que pueda entrar algún lector
+    if (escrib || tam_cola_lec == 0)
         lectura.wait();
 
-    assert(escrib == false);
+    assert(escrib == false && tam_cola_lec>0);
 
-    // hacer la operación de lectura, actualizando estado del monitor
+    // comenzar la operación de lectura
     n_lec++;
+
+    // si hay algún escritor en la cola para escribir
     if (!(escritura.empty())){
-        cuanto_leo--;
-        //cout << "Hay escritores esperando " << endl;
+        tam_cola_lec--;
     }
-       
 
     // señalar al productor que hay un hueco libre, por si está esperando
-    if (cuanto_leo > 0)
+    if (tam_cola_lec > 0)
         lectura.signal();
 }
 
 // -----------------------------------------------------------------------------
-// función llamada por el lector para finalizar lectura
+// Función llamada por el lector para finalizar lectura
 
 void LecEscMU1::fin_lectura()
 {
-    // esperar bloqueado hasta que 0 < n_lec
     n_lec--;
 
     assert(escrib == false);
 
+    // si es el último lector desbloquear un escritor
     if (n_lec == 0)
     {
         escritura.signal();
-        cuanto_leo = 5;
+        tam_cola_lec = 5;
     }
 }
 
 // -----------------------------------------------------------------------------
+// Función llamada por el escritor para iniciar la escritura
 
 void LecEscMU1::ini_escritura()
 {
-    // esperar bloqueado hasta que 0 < n_lec
+    // esperar bloqueado hasta que n_lec == 0 y no haya nadie escribiendo
     if (escrib || n_lec > 0)
         escritura.wait();
 
     assert(escrib == false && n_lec == 0);
 
-    // hacer la operación de lectura, actualizando estado del monitor
+    // registrar que hay un escritor
     escrib = true;
 }
 
 // -----------------------------------------------------------------------------
-// función llamada por el lector para finalizar lectura
+// función llamada por el escritor para finalizar escritura
 
 void LecEscMU1::fin_escritura()
 {
-    // esperar bloqueado hasta que 0 < n_lec
+    // registrar que ya no hay escritor
     escrib = false;
 
     assert(n_lec == 0);
 
+    // Si hay lectores, despertar uno
+    // si no hay, despertar un escritor
     if (!(n_lec == 0))
         lectura.signal();
     else
@@ -147,7 +131,7 @@ void LecEscMU1::fin_escritura()
 }
 
 // *****************************************************************************
-// funciones de hebras
+// Funciones de las hebras
 
 void funcion_hebra_lectora(MRef<LecEscMU1> monitor, int index)
 {
@@ -185,11 +169,12 @@ int main()
          << "--------------------------------------------------------------------" << endl
          << flush;
 
-    pergamino[0] = 0; // inicialización del generador de datos
+    // inicializar el generador de datos
+    pergamino[0] = 0;
     for (int i = 1; i < num_items; i++)
-        pergamino[i] = i; // inicialización del generador de datos
+        pergamino[i] = i;
 
-    // crear monitor  ('monitor' es una referencia al mismo, de tipo MRef<...>)
+    // crear monitor
     MRef<LecEscMU1> monitor = Create<LecEscMU1>();
 
     // crear y lanzar las hebras
