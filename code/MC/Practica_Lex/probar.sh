2493242c92cb9236b1cc36b87ceeff2aa5d003d1
jesusmuve18 <jesumuve19@gmail.com>
2024-12-28 16:55:50 +0100

    Actualizado 28.12.2024 16:55

--- code/MC/Practica_Lex/probar.sh
+++ code/MC/Practica_Lex/probar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-script_file=lex
+script_file=html-to-latex
 text_file=fichero.html
 output_file=salida.tex
 
-flex++ ./$script_file.l
+flex++ -o ./$script_file.yy.cc ./$script_file.l
 g++ -o ./$script_file ./$script_file.yy.cc
 ./$script_file ./$text_file > ./$output_file
 # pdflatex ./$output_file
